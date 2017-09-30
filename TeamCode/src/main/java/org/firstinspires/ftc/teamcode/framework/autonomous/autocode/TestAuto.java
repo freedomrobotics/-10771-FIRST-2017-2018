00d83701e8eb2981cb7c11deb3c0517ae93f7032
Kevin Raneri <kevin.raneri@gmail.com>
2017-09-30 11:49:12 -0400

    Change drive code in autonomous to reflect new drivetrain design and formula.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/autonomous/autocode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/autonomous/autocode/TestAuto.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.framework.autonomous.autocode;
 
 import org.firstinspires.ftc.teamcode.framework.autonomous.AutoCode;
 import org.firstinspires.ftc.teamcode.framework.autonomous.Autonomous;
+import org.firstinspires.ftc.teamcode.framework.autonomous.components.AutoComponents;
 
 /**
  * Created by Kevin on 9/1/2017.
@@ -14,10 +15,6 @@ public class TestAuto extends AutoProgram {
 
     @Override
     public void run() throws InterruptedException {
-        hardware.right.setPower(1);
-        hardware.left.setPower(1);
-        Thread.sleep(1000);
-        hardware.right.setPower(0);
-        hardware.left.setPower(0);
+        AutoComponents.driveTrain.forward(1000); //drive forward for 1 second
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/autonomous/components/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/autonomous/components/DriveTrain.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode.framework.autonomous.components;
 
 import org.firstinspires.ftc.teamcode.framework.autonomous.Autonomous;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Kevin on 9/15/2017.
  */
@@ -14,5 +18,61 @@ public class DriveTrain extends Component {
     @Override
     public void init() {
         //initialize drivetrain parameters here
+    }
+
+    public void forward (int ms) throws InterruptedException {
+        DriveParams params = calcParams(1, 0, 0);
+        drive(params.vD, params.thetaD, params.vTheta);
+        Thread.sleep(ms);
+        DriveParams stop = calcParams(0, 0, 0);
+        drive(stop.vD, stop.thetaD, stop.vTheta);
+    }
+
+    public DriveTrain.DriveParams calcParams(double frontback, double leftright, double turnspeed) {
+        double vD = Math.sqrt(Math.pow(frontback, 2)+Math.pow(leftright, 2));
+        double thetaD = Math.atan2(frontback, leftright);
+        double vTheta = turnspeed;
+
+        return new DriveParams(vD, thetaD, vTheta);
+    }
+
+    public void drive(double vD, double thetaD, double vTheta) {
+        //calculate powers (that's some fun trig stuff)
+        double frontleft = vD * Math.sin(thetaD + Math.PI / 4) + vTheta;
+        double frontright = vD * Math.cos(thetaD + Math.PI / 4) - vTheta;
+        double backleft = vD * Math.cos(thetaD + Math.PI / 4) + vTheta;
+        double backright = vD * Math.sin(thetaD + Math.PI / 4) - vTheta;
+
+        //clamp motor speed values
+
+        List<Double> powers = Arrays.asList(frontleft, frontright, backleft, backright);
+        double minPower = Collections.min(powers);
+        double maxPower = Collections.max(powers);
+        double maxMag = Math.max(Math.abs(minPower), Math.abs(maxPower));
+
+        if (maxMag > 1.0) {   //we need to scale these down, they are too big, and will cause errors!
+            for (int i = 0; i < powers.size(); i++) {
+                powers.set(i, powers.get(i) / maxMag); //will scale the largest to 1, and the smaller ones proportionally.
+            }
+        }
+
+        //set all motor powers using the values stored in the powers list.
+        hardware.frontLeft.setPower(powers.get(0));
+        hardware.frontRight.setPower(powers.get(1));
+        hardware.backLeft.setPower(powers.get(2));
+        hardware.backRight.setPower(powers.get(3));
+    }
+
+    public class DriveParams {
+
+        public double vD;
+        public double thetaD;
+        public double vTheta;
+
+        public DriveParams(double vD, double thetaD, double vTheta) {
+            this.vD = vD;
+            this.thetaD = thetaD;
+            this.vTheta = vTheta;
+        }
     }
 }
